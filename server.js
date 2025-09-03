681b32efa854ff4d5b975e8a3e4774a3ef906b7d
rusllanhidayatt <rusllanhidayat@gmail.com>
2025-09-03 21:10:23 +0700

    fix my Apps

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 const authRoute = require("./routes/authRoute");
 const itemsRoute = require("./routes/itemsRoute");
-const uploadRoute = require("./routes/uploadRoute");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,11 +15,10 @@ app.use(bodyParser.json({ limit: "2mb" }));
 // Routes
 app.use("/login", authRoute); // POST /login
 app.use("/items", itemsRoute); // CRUD items
-app.use("/upload", uploadRoute); // POST /upload
 
 // Health / root
 app.get("/", (_req, res) => {
-res.send("API running! Use /login, /items, /upload, /import");
+res.send("API running! Use /login, /items, /import");
 });
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
